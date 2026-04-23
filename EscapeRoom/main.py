8504925e511750f634921d2d1cab9d4de98c348a
hephz-pekun <iyanuoluwa.olanipek@bison.howard.edu>
2026-04-23 12:55:41 -0400

    Unit testings

---
+++ EscapeRoom/main.py
@@ -0,0 +1,16 @@
+from flask import Flask, send_from_directory
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home():
+    return send_from_directory(".", "index.html")
+
+@app.route("/<path:path>")
+def static_files(path):
+    return send_from_directory(".", path)
+
+if __name__ == "__main__":
+    app.run(debug=True)
