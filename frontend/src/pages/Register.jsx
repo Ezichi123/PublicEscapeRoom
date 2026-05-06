ca06a602c05aa23b3e83f4822a62a3ca1c5cd69d
Ezichi123 <141843866+Ezichi123@users.noreply.github.com>
2026-05-06 13:27:22 -0400

    updated urls

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -21,7 +21,7 @@ export default function Register() {
       return
     }
 
-    const res = await fetch('http://127.0.0.1:8000/api/users/register/', {
+    const res = await fetch('${import.meta.env.VITE_API_URL}/api/users/register/', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ username, password })
